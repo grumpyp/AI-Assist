ee31b401c142c2bba34012492a9d0faff5e1ac78
Julius Lipp <julius_lipp@yahoo.de>
2022-12-10 18:52:45 +0100

    Some constrain changes

--- backend/src/database/models/call_analysis.py
+++ backend/src/database/models/call_analysis.py
@@ -5,9 +5,9 @@ from database.db import db
 
 
 class CallAnalysis(db.Model):
-    id = db.Column(db.String(36), primary_key=True, default=uuid.uuid4)
-    call_id = db.Column(db.String(36), db.ForeignKey("call.id"))
-    call = db.relationship("Call", lazy=True)
+    id = db.Column(db.String(36), primary_key=True, default=lambda: str(uuid.uuid4()))
+    call_id = db.Column(db.String(36), db.ForeignKey("call.id"), nullable=True)
+    call = db.relationship("Call", lazy=True, viewonly=True)
     sentiment = db.Column(db.Float)
     keywords = db.Column(db.JSON)
     entities = db.Column(db.JSON)
--- backend/src/database/models/employees/employee.py
+++ backend/src/database/models/employees/employee.py
@@ -5,32 +5,32 @@ from datetime import datetime
 from database.db import db
 
 employee_call = db.Table('employee_call',
-                         db.Column('call_id', db.String(36), db.ForeignKey('call.id')),
+                         db.Column('call_id', db.String(36), db.ForeignKey('call.id'), nullable=True),
                          db.Column('employee_id', db.String(36), db.ForeignKey('employee.id')))
 
 employee_team = db.Table('employee_team',
-                         db.Column('team_id', db.String(36), db.ForeignKey('team.id')),
+                         db.Column('team_id', db.String(36), db.ForeignKey('team.id'), nullable=True),
                          db.Column('employee_id', db.String(36), db.ForeignKey('employee.id')))
 
 employee_queueing_theory_best_match = db.Table('employee_queueing_theory_best_match',
                                                db.Column('queueing_theory_best_match:id', db.String(36),
-                                                         db.ForeignKey('queueing_theory_best_match.id')),
+                                                         db.ForeignKey('queueing_theory_best_match.id'), nullable=True),
                                                db.Column('employee_id', db.String(36), db.ForeignKey('employee.id')))
 
 
 class Employee(db.Model):
-    id = db.Column(db.String(36), primary_key=True, default=uuid.uuid4)
+    id = db.Column(db.String(36), primary_key=True, default=lambda: str(uuid.uuid4()))
     name = db.Column(db.String(255))
     contact_info = db.Column(db.String(255))
     role = db.Column(db.Enum("employee", "manager", "leader"))
-    profile_picture = db.Column(db.LargeBinary)
+    profile_picture = db.Column(db.LargeBinary, nullable=True)
     description = db.Column(db.Text)
     created_at = db.Column(db.DateTime, default=datetime.utcnow)
     updated_at = db.Column(db.DateTime, default=datetime.utcnow)
-    calls = db.relationship("Call", secondary=employee_call)
-    teams = db.relationship("Team", secondary=employee_team)
+    calls = db.relationship("Call", secondary=employee_call, viewonly=True)
+    teams = db.relationship("Team", secondary=employee_team, viewonly=True)
     queueing_theory_best_match = db.relationship("QueueingTheoryBestMatch",
-                                                 secondary=employee_queueing_theory_best_match)
+                                                 secondary=employee_queueing_theory_best_match, viewonly=True)
 
     @abstractmethod
     def get_performance_metrics(self):
