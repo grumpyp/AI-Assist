d73f9cc3e29f094526725b9c45c1d134975b388c
grumpyp <info@gerryontour.de>
2022-12-10 01:42:32 +0100

    WIP: backend initial setup

---
+++ backend/src/views/call.py
@@ -0,0 +1,20 @@
+from flask import Blueprint
+from ..database.models import Call, db
+
+call_blueprint = Blueprint('call', __name__)
+
+
+@call_blueprint.route('/')
+def all_calls():
+    calls = Call.query.all()
+    return calls
+
+@call_blueprint.route('<int:call_id>')
+def filter_by_id(**kwargs):
+    call = Call.filter_by(id=call_id).first()
+    return call
+
+@call_blueprint.route('/insert')
+def insert():
+    call = Call(id=2)
+    return "inserted"
