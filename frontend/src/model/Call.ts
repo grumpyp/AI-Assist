7f0151834b94fda7440f9e6e49ddbcfb5708dad4
grumpyp <info@gerryontour.de>
2022-12-11 12:07:32 +0100

    add Call model into frontend

---
+++ frontend/src/model/Call.ts
@@ -0,0 +1,27 @@
+export interface Call {
+  callback_requested: boolean;
+  created_at: string;
+  customer: {
+    account_number: string;
+    contact_info: string;
+    id: string;
+    location: string;
+    name: string;
+    preferences: {
+      sms: boolean;
+    };
+  };
+  customer_id: string;
+  faqs: any[];
+  feedback: string;
+  id: string;
+  language: string;
+  problems: Problem[];
+  recordings: Recording[];
+  solutions: Solution[];
+  status: string;
+  transcript_requested: boolean;
+  transfer_requested: boolean;
+  updated_at: string;
+  voicemail_left: boolean;
+}
