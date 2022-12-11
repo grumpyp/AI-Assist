7d49d317df907253ce93e6ccf6b5b527363aa3d8
grumpyp <info@gerryontour.de>
2022-12-11 12:48:06 +0100

    add Problem model frontend and faq mock route backend

---
+++ frontend/src/model/Problem.ts
@@ -0,0 +1,10 @@
+export interface Problem {
+  call_id: string;
+  category: string;
+  created_at: string;
+  id: string;
+  long_description: string;
+  summary: string;
+  type: string;
+  updated_at: string;
+}
